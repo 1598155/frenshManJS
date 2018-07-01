9be28af131f3bbba562fe803f68b70993ebb6810
liuwei <970403487@qq.com>
2018-07-01 18:30:09 +0800

    数组的使用，简单排序 https://github.com/1598155/frenshManJS/issues/2

--- src/main/java/webapps/test.js
+++ src/main/java/webapps/test.js
@@ -14,3 +14,38 @@ function bubble()
 	
 	}
 
+function sortBubble()
+{
+	var listNumber= new Array(1,3,9,7,5,4,3);
+	for(var i=0;i<listNumber.length;i++)
+	{
+		
+		for(var j=0;j<listNumber.length-i-1;j++)
+		{
+			if(listNumber[j]>listNumber[j+1])
+			{
+				  var temp =listNumber[j];
+				  listNumber[j]= listNumber[j+1];
+				  listNumber[j+1]= temp;
+
+				}
+			
+		}
+		
+	}
+	alert(listNumber);
+
+}
+
+function swap(x,y)
+{
+  var temp = x;
+  x = y;
+  y = temp;
+
+}
+
+
+
+
+
